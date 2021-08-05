8db128243f1952cc96f0c5a45ab8c55093b15759
natelunt <nate.lunt04@gmail.com>
2021-08-04 19:16:54 -0700

    Continued work on trails.js component

--- src/components/Trails/Trails.js
+++ src/components/Trails/Trails.js
@@ -7,14 +7,29 @@ function Trails(props) {
     const [ trails, setTrails ] = useState([]);
     const [ filteredTrails, setFilteredTrails ] = useState([]);
     const [ usingFilter, setUsingFilter ] = useState(false);
-    const [ trailRanks, setTrailRanks ] = useState([]);
-    const [ rankFilterOpen, setRankFilterOpen ] = useState(false);
     const [ trailLocations, setTrailLocations ] = useState([]);
     const [ trailLocationFilterOpen, setTrailLocationFilterOpen ] = useState(false);
-    const [ trailNames, setTrailNames ] = useState([]);
-    const [ nameFilterOpen, setNameFilterOpen ] = useState(false);
 
-    useEffect()
+    useEffect(() => {
+        axios.get('/api/trails').then(res => {
+            console.log(res.data)
+            let tempTrails = res.data
+            let sortedTrails = tempTrails.sort((a, b) => {
+                return a.location > b.location ? 1 : -1
+            })
+            setTrails(sortedTrails)
+        }).catch(error => {
+            console.log(error)
+        })
+        axios.get('/api/trails').then(res => {
+            let tempTrailLocations = res.data.sort((a, b) => {
+                return a.location > b.location ? 1 : -1
+            })
+            setTrailLocations(tempTrailLocations)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [])
 
     function toggleUsingFilter() {}
 }
