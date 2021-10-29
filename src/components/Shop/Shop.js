ffaedca1afd278882c1ab72532043a3767d130c0
natelunt <nate.lunt04@gmail.com>
2021-10-28 20:42:44 -0700

    began shop, need to finish category.js & searchbar.js

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import axios from 'axios';
-// import './shop.css';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import ProductList from '../Products/ProductList';
+// import Search from '../../assets/Search/SearchBar';
+import Categories from '../../assets/Categories/Categories';
+import { getProducts, getProductsByCategory } from '../../redux/productsReducer'
+import './shop.css';
 
 
-const Shop = () => {
+
+const Shop = ({ getProducts, getProductsByCategory }) => {
     return (
         <div>
             shop
