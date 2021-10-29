d8282ee89535c5052e95b712e01f4a445cd41514
natelunt <nate.lunt04@gmail.com>
2021-10-28 21:02:25 -0700

    began building searchbar, need to complete formInput.js

---
+++ src/assets/Search/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import FormInput from '../FormInput/FormInput';
+import './searchBar.css';
+
+const SearchBar = ({ searchValue, setSearchValue }) => {
+    return (
+        <section></section>
+    )
+};
+
+export default SearchBar;
