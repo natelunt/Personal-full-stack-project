c933e2f6abc2bcf94485c7b9410c2f77730e857f
natelunt <nate.lunt04@gmail.com>
2021-11-03 20:58:49 -0700

    wrote nav.js, needs testing, styling may change

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,22 +2,47 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './nav.css';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-// import { registerUser } from '../redux/userReducer';
+import { Link, useHistory } from 'react-router-dom';
+import { clearUser } from '../../redux/userReducer';
+import Button from '../../assets/Button/Button'
 
 
-const Nav = () => {
-    // const [isLoggedIn, setIsLoggedIn] = useState(false);
+const Nav = ({ user, clearUser, cartItems }) => {
+    const history = useHistory();
+    
+    const logout = async () => {
+        try {
+            await axios.delete('/api/logout');
+            clearUser();
+            history.push('/logout');
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
     return (
-        <div className='nav'>
-            <div className='nav-links'>
-                <Link to='/explore'><button className='explore-button'>explore</button></Link>
-                <Link to='/plan'><button className='plan-button'>plan</button></Link>
-            </div>
-            <Link to='/home'><div className='nav-img'>On The Rocks</div></Link>
-            <Link to='/menu'><button className='nav-avatar'>avatar</button></Link>
-        </div>
-    )
+        <nav>
+            <ul classname='nav-links'>
+                <Link to='/'>Home</Link>
+                {user.email && <Link to={`/profile/${user.user_id}`}>Profile</Link>}
+                {!user.email && <Link to='/'>Login/Register</Link>}
+                <Link to='/shop'>Shop</Link>
+                <Link to='/cart'>Cart ({cartItems.reduce((acc, item) => acc + item.qty, 0)})</Link>
+                {user.email && <Button styleName='logout' label='Logout' handleClick={logout}/>}
+            </ul>
+        </nav>
+    );
+};
+
+const mapDispatchToProps = {
+    clearUser: clearUser
+};
+
+const mapStateToProps = (reduxState) => {
+    return {
+        user: reduxState.userReducer.user,
+        cartItems: reduxState.cartReducer.cartItems
+    }
 }
 
-export default Nav
+export default connect(mapStateToProps, mapDispatchToProps)(Nav);
